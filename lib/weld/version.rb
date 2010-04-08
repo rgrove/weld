2164d7209b107ba666acfc3cd055927e27d70ce4
Ryan Grove <ryan@wonko.com>
2010-04-08 14:27:55 -0700

    Version bump.

--- lib/weld/version.rb
+++ lib/weld/version.rb
@@ -1,6 +1,6 @@
 class Weld
   APP_NAME      = 'Weld'
-  APP_VERSION   = '0.0.1.dev.20100404'
+  APP_VERSION   = '0.0.1.dev.20100408'
   APP_AUTHOR    = 'Ryan Grove'
   APP_EMAIL     = 'ryan@wonko.com'
   APP_URL       = 'http://wonko.com/'
